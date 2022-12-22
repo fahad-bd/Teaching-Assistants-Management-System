50a86914748a8c91ee7c8fed2c21cf526a16c7c1
Fahad <fahadahammed1@gmail.com>
2022-12-22 19:05:43 +0600

    end

--- js/main.js
+++ js/main.js
@@ -60,4 +60,3 @@ const hideSidebar = () => {
 showSidebarBtn.addEventListener('click', showSidebar);
 hideSidebarBtn.addEventListener('click', hideSidebar);
 //--------------------- End Start Manage Categori ----------------------//
-
