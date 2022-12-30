bb56289a270d2600c052fbcd517c8298251399c4
Fahad <fahadahammed1@gmail.com>
2022-12-30 18:07:08 +0600

    maked connecton with database

--- config/constants.php
+++ config/constants.php
@@ -1,3 +1,7 @@
 <?php
 define('ROOT_URL','http://localhost/Fahad/TAMS/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin');
+define('DB_PASS', 'admin123');
+define('DB_NAME', 'taDB');
 ?>
