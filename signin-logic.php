b4d4830f6020b74e41a7b959d080f446f4fd51ea
Fahad <fahadahammed1@gmail.com>
2022-12-31 17:23:19 +0600

    create connection with db and signin page

---
+++ signin-logic.php
@@ -0,0 +1,60 @@
+<?php
+require 'config/database.php';
+
+//check button cleck or not
+if(isset($_POST['submit'])){
+    //get form data
+    $username_email = filter_var($_POST['username_email'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    //check every box is fillup or not
+    if(!$username_email){
+        $_SESSION['signin'] = "Enter Username or Email.";
+    }
+    else if(!$password){
+        $_SESSION['signin'] = "Password Required!";
+    }
+    else {
+        // fetch user from database
+        $fetch_user_query = "SELECT * FROM users WHERE username = '$username_email' OR email = '$username_email'";
+        $fetch_user_result = mysqli_query($connection, $fetch_user_query);
+
+        if(mysqli_num_rows($fetch_user_result) == 1){
+            // we found a valid user
+            // conver the DB row into a array
+            $user_record = mysqli_fetch_assoc($fetch_user_result);
+            $db_password = $user_record['password'];
+            // compare form password with DB stored password
+            if(password_verify($password, $db_password)){
+                // set session for access control
+                $_SESSION['user-id'] = $user_record['id'];
+                // set session if user is an admin
+                if($user_record['designation'] == 1){
+                    $_SESSION['user_is_admin'] = true;
+                }
+
+                // log user in
+                header('location: ' . ROOT_URL . 'admin/');
+            }
+            else {
+                $_SESSION['signin'] = "Password or Email donot match. Try again!";
+
+            }
+        }
+        else {
+            $_SESSION['signin'] = "User not found!";
+        }
+    }
+
+    // if any problem in box of form, redirect back to signin page with login data
+    if(isset($_SESSION['signin'])){
+        $_SESSION['signin-data'] = $_POST;
+        header('location: ' . ROOT_URL . 'signin.php');
+        die();
+    }
+}
+else {
+    header('location: ' . ROOT_URL . 'signin.php');
+    die();
+}
+?>
--- signin.php
+++ signin.php
@@ -1,5 +1,11 @@
 <?php
 require 'config/constants.php';
+
+$username_email = $_SESSION['signin-data']['username_email'] ?? null;
+$password = $_SESSION['signin-data']['password'] ?? null;
+
+unset($_SESSION['signin-data']);
+
 ?>
 
 <!DOCTYPE html>
@@ -29,13 +35,27 @@ require 'config/constants.php';
     <section class="form__section">
         <div class="container form__section-container">
             <h2>Sign In</h2>
-            <div class="alert__message success">
-                <p>This is an success message</p>
-            </div>
-            <form action="">
-                <input type="text" placeholder="Username or Email">
-                <input type="password" placeholder="Password">
-                <button class="btn" type="submit">Sign In</button>
+            <?php if(isset($_SESSION['signup-success'])) : ?>
+                <div class="alert__message success">
+                    <p>
+                        <?= $_SESSION['signup-success'];
+                        unset($_SESSION['signup-success']); 
+                        ?>
+                    </p>
+                </div>
+            <?php elseif(isset($_SESSION['signin'])) : ?>    
+                <div class="alert__message error">
+                    <p>
+                        <?= $_SESSION['signin'];
+                        unset($_SESSION['signin']); 
+                        ?>
+                    </p>
+                </div>
+            <?php endif ?>    
+            <form action="<?= ROOT_URL ?>signin-logic.php" method="POST">
+                <input type="text" name="username_email" value="<?= $username_email ?>" placeholder="Username or Email">
+                <input type="password" name="password" value="<?= $password ?>" placeholder="Password">
+                <button class="btn" name="submit" type="submit">Sign In</button>
                 <!-- <small>Don't have an account? <a href="signup.html">Sign Up</a></small> -->
             </form>
         </div>
