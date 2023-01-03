3d15944e7b7021fdaf83f7cc7dcb858d4ecbcf8c
Fahad <fahadahammed1@gmail.com>
2023-01-03 12:43:07 +0600

    title and description from db

--- admin/edit-category.php
+++ admin/edit-category.php
@@ -1,5 +1,21 @@
 <?php
 include 'partials/header.php';
+
+if(isset($_GET['id'])){
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+    //fetch category from db
+    $query = "SELECT * FROM categories WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+
+    if(mysqli_num_rows($result) == 1){
+        $category = mysqli_fetch_assoc($result);
+    }
+}
+else {
+    header('location: ' . ROOT_URL . 'admin/manage-categories.php');
+    die();
+}
 ?>
 
 
@@ -8,8 +24,8 @@ include 'partials/header.php';
         <div class="container form__section-container">
             <h2>Edit Category</h2>
             <form action="">
-                <input type="text" placeholder="Title">
-                <textarea rows="4" placeholder="Description"></textarea>
+                <input type="text" value="<?= $category['title'] ?>" placeholder="Title">
+                <textarea rows="4" placeholder="Description"> <?= $category['description'] ?></textarea>
                 <button class="btn" type="submit">Update Category</button>
             </form>
         </div>
