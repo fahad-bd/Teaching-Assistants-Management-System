98d6a2615f49004e28e16a051ec46a4fcdeae775
Fahad <fahadahammed1@gmail.com>
2023-01-04 00:54:40 +0600

    post are shows from db

--- admin/add-post.php
+++ admin/add-post.php
@@ -4,6 +4,14 @@ include 'partials/header.php';
 //fetch categories from db
 $query = "SELECT * FROM categories";
 $categories = mysqli_query($connection, $query);
+
+// get back form data if error occors
+$title = $_SESSION['add-post-data']['title'] ?? null;
+$body = $_SESSION['add-post-data']['body'] ?? null;
+
+
+//delete form data session
+unset($_SESSION['add-post-data']);
 ?>
 
 
@@ -11,17 +19,23 @@ $categories = mysqli_query($connection, $query);
     <section class="form__section">
         <div class="container form__section-container">
             <h2>Create Post</h2>
-            <!-- <div class="alert__message error">
-                <p>This is an error message</p>
-            </div> -->
+            <?php if(isset($_SESSION['add-post'])) : ?>
+                <div class="alert__message error">
+                    <p>
+                        <?= $_SESSION['add-post'];
+                        unset($_SESSION['add-post']);
+                        ?>
+                    </p>
+                </div>
+            <?php endif ?>
             <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
-                <input type="text" name="title" placeholder="Title">
+                <input type="text" name="title" value="<?= $title ?>" placeholder="Title">
                 <select name="category">
                     <?php while($category = mysqli_fetch_assoc($categories)) : ?>
                     <option value="<?= $category['id']?>"><?= $category['title'] ?></option>
                     <?php endwhile ?>
                 </select>
-                <textarea rows="10" name="body" placeholder="Body"></textarea>
+                <textarea rows="10" name="body" placeholder="Body"><?= $body ?></textarea>
 
 
                 <?php if(isset($_SESSION['user_is_admin'])) : ?>
--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,27 @@
 <?php
 include 'partials/header.php';
+
+//fetch current user's posts from db
+$current_user_id = $_SESSION['user-id'];
+// $query = "SELECT posts.id, posts.title, posts.category_id FROM posts JOIN users ON posts.author_id = users.id WHERE posts.author_id = $current_user_id ORDER BY posts.id DESC";
+$query = "SELECT id, title, category_id FROM posts WHERE author_id = $current_user_id ORDER BY id DESC";
+$posts = mysqli_query($connection, $query);
 ?>
 
 
     <!----------------------------------------- Start Manage Post --------------------------------------->
     <section class="dashboard">
+
+    <?php if(isset($_SESSION['add-post-success'])) : ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['add-post-success'];
+                unset($_SESSION['add-post-success']);
+                ?>
+            </p>
+        </div>
+    <?php endif ?>
+
         <div class="container dashboard__container">
             <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
             <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
@@ -40,7 +57,8 @@ include 'partials/header.php';
                 </ul>
             </aside>
             <main>
-                <h2>Manage Users</h2>
+                <h2>Manage Post</h2>
+                <?php if(mysqli_num_rows($posts) > 0) : ?>
                 <table>
                     <thead>
                         <tr>
@@ -50,27 +68,29 @@ include 'partials/header.php';
                             <th>Delete</th>
                         </tr>
                     </thead>
-                    <body>
+                    <tbody>
+                        <?php while($post = mysqli_fetch_assoc($posts)) : ?>
+                            <!-- get category title of each post from category table  -->
+                            <?php
+                            $category_id = $post['category_id'];
+                            $category_query = "SELECT title FROM categories WHERE id = $category_id";
+                            $category_result = mysqli_query($connection, $category_query);
+                            $category = mysqli_fetch_assoc($category_result);
+                            ?>
                         <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing.</td>
-                            <td>Art</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-post.php" class="btn sm danger">Delete</a></td>
+                            <td><?= $post['title'] ?></td>
+                            <td><?= $category['title'] ?></td>
+                            <td><a href="<?= ROOT_URL ?>admin/edit-post.php?id=<?= $post['id']?>" class="btn sm">Edit</a></td>
+                            <td><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id']?>" class="btn sm danger">Delete</a></td>
                         </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing.</td>
-                            <td>Art</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-post.php" class="btn sm danger">Delete</a></td>
-                        </tr>
-                        <tr>
-                            <td>Lorem ipsum dolor sit amet consectetur adipisicing.</td>
-                            <td>Art</td>
-                            <td><a href="edit-post.php" class="btn sm">Edit</a></td>
-                            <td><a href="delete-post.php" class="btn sm danger">Delete</a></td>
-                        </tr>
-                    </body>
+                        <?php endwhile ?>
+                    </tbody>
                 </table>
+                <?php else : ?>
+                    <div class="alert__message error">
+                        <?= "No post found!" ?>
+                    </div>
+                <?php endif ?>   
             </main>
         </div>
     </section>
