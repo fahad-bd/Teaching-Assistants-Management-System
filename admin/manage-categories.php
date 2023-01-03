95221855bd28afba965d631d3d30f6ab8cd74e6a
Fahad <fahadahammed1@gmail.com>
2023-01-03 13:30:54 +0600

    edit category confirm massage

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -29,6 +29,22 @@ $categories = mysqli_query($connection, $query);
                         ?>
                     </p>
             </div>
+    <?php elseif(isset($_SESSION['edit-category'])) : ?>
+            <div class="alert__message error container">
+                    <p>
+                        <?= $_SESSION['edit-category'];
+                        unset($_SESSION['edit-category']); 
+                        ?>
+                    </p>
+            </div>
+    <?php elseif(isset($_SESSION['edit-category-success'])) : ?>
+            <div class="alert__message success container">
+                    <p>
+                        <?= $_SESSION['edit-category-success'];
+                        unset($_SESSION['edit-category-success']); 
+                        ?>
+                    </p>
+            </div>
     <?php endif ?>
 
         <div class="container dashboard__container">
