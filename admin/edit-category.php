55d059e409405f74030b6a1be6cc7e285be63117
Fahad <fahadahammed1@gmail.com>
2023-01-03 13:26:57 +0600

    db connection with update category done

--- admin/edit-category.php
+++ admin/edit-category.php
@@ -23,10 +23,11 @@ else {
     <section class="form__section">
         <div class="container form__section-container">
             <h2>Edit Category</h2>
-            <form action="">
-                <input type="text" value="<?= $category['title'] ?>" placeholder="Title">
-                <textarea rows="4" placeholder="Description"> <?= $category['description'] ?></textarea>
-                <button class="btn" type="submit">Update Category</button>
+            <form action="<?= ROOT_URL ?>admin/edit-category-logic.php" method="POST">
+                <input type="hidden" name="id" value="<?= $category['id'] ?>" >
+                <input type="text" name="title" value="<?= $category['title'] ?>" placeholder="Title">
+                <textarea rows="4" name="description" placeholder="Description"> <?= $category['description'] ?></textarea>
+                <button type="submit" name="submit" class="btn">Update Category</button>
             </form>
         </div>
     </section>
