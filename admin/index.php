2df36e174306e9126ff1cc09e9f0b6faf68a2956
Fahad <fahadahammed1@gmail.com>
2023-01-04 21:23:56 +0600

    delete success added

--- admin/index.php
+++ admin/index.php
@@ -36,6 +36,14 @@ $posts = mysqli_query($connection, $query);
                 ?>
             </p>
         </div>
+    <?php elseif(isset($_SESSION['delete-post-success'])) : ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['delete-post-success'];
+                unset($_SESSION['delete-post-success']);
+                ?>
+            </p>
+        </div>
     <?php endif ?>
 
         <div class="container dashboard__container">
