fb72d1f9350293a94df9a85d9784944bfbbd5214
Fahad <fahadahammed1@gmail.com>
2023-01-14 18:54:46 +0600

    delete catagories list from some page

--- tj.php
+++ tj.php
@@ -50,17 +50,7 @@ include 'partials/header.php'
     <!--------------------------------------- End Post Body ----------------------------------->
 
     <!--------------------------------------- Start Category ----------------------------------->
-    <section class="category__buttons">
-        <div class="container category__buttons-container">
-            <?php
-                $all_categories_query = "SELECT * FROM categories";
-                $all_categories = mysqli_query($connection, $all_categories_query);
-            ?>
-            <?php while($category = mysqli_fetch_assoc($all_categories)) : ?>
-            <a href="<?= ROOT_URL ?>category-posts.php?id=<?= $category['id'] ?>" class="category__button"><?= $category['title'] ?></a>
-            <?php endwhile ?>
-        </div>
-    </section>
+    
     <!--------------------------------------- End Category ----------------------------------->
 
 <?php
